4534f2499b783c4516969ee997e6259b171dbb92
Tomas Urbanavičius <tomas.urbanavicius@stormgeo.com>
2023-12-01 08:51:07 +0200

    Added script

---
+++ script.js
@@ -0,0 +1,67 @@
+﻿const observeDOM = (function () {
+    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+    return function (obj, callback) {
+        if (!obj || obj.nodeType !== 1) return;
+
+        if (MutationObserver) {
+            // define a new observer
+            var mutationObserver = new MutationObserver(callback)
+
+            // have the observer observe for changes in children
+            mutationObserver.observe(obj, {childList: true, subtree: true})
+            return mutationObserver
+        }
+
+        // browser support fallback
+        else if (window.addEventListener) {
+            obj.addEventListener('DOMNodeInserted', callback, false)
+            obj.addEventListener('DOMNodeRemoved', callback, false)
+        }
+    }
+})();
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elem = document.getElementById('api');
+    changeElements();
+    observeDOM(elem, function (m) {
+        var addedNodes = [], removedNodes = [];
+        m.forEach(record => record.addedNodes.length & addedNodes.push(...record.addedNodes))
+
+        m.forEach(record => record.removedNodes.length & removedNodes.push(...record.removedNodes))
+
+        changeElements();
+        console.log('Added:', addedNodes, 'Removed:', removedNodes);
+    });
+});
+
+
+function changeElements() {
+    //Change email input to div
+    const input = document.getElementById('signInName');
+    if (input) {
+        const email = input.getAttribute('value');
+        if (input.innerHTML !== email) {
+            if (email) {
+                input.outerHTML = '<div class="username">' + email + '</div>';
+            }
+        }
+    }
+
+    //Move forgot password link
+    const passwordLabel = document.getElementsByClassName('password-label')[0];
+    if (passwordLabel && passwordLabel.parentElement.firstElementChild === passwordLabel) {
+        passwordLabel.parentElement.appendChild(passwordLabel);
+    }
+
+    //Add back button
+    const nextButton = document.getElementById('next');
+    const backButton = document.getElementsByClassName('back-button');
+    if (nextButton && backButton.length === 0) {
+        const newButton = document.createElement('button');
+        newButton.className = 'back-button';
+        newButton.innerHTML = '<div class="back-button-text" onclick="history.back()"><span class="material-icons back-icon">arrow_back_ios</span>Back</div>';
+        nextButton.parentElement.appendChild(newButton);
+        nextButton.parentElement.appendChild(nextButton);
+    }
+}
