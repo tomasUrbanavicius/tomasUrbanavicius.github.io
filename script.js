7753359f435b6db57186ebc98649ab1a916648ad
Tomas Urbanavičius <tomas.urbanavicius@stormgeo.com>
2023-12-04 16:17:25 +0200

    Change

--- script.js
+++ script.js
@@ -2,22 +2,27 @@
     const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
     return function (obj, callback) {
-        if (!obj || obj.nodeType !== 1) return;
+        if (!obj || obj.nodeType !== 1) {
+            return;
+        }
 
         if (MutationObserver) {
             // define a new observer
-            var mutationObserver = new MutationObserver(callback)
+            const mutationObserver = new MutationObserver(callback);
 
             // have the observer observe for changes in children
-            mutationObserver.observe(obj, {childList: true, subtree: true})
-            return mutationObserver
+            mutationObserver.observe(obj, {
+                attributeFilter: ['style'],
+                attributes: true,
+                childList: true,
+                subtree: true,
+            });
+            return mutationObserver;
         }
-
         // browser support fallback
         else if (window.addEventListener) {
-            obj.addEventListener('DOMNodeInserted', callback, false)
-            obj.addEventListener('DOMNodeRemoved', callback, false)
-            obj.addEventListener('DOMCharacterDataModified', callback, false)
+            obj.addEventListener('DOMNodeInserted', callback, false);
+            obj.addEventListener('DOMNodeRemoved', callback, false);
         }
     }
 })();
